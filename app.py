d59b120d4269ab4801d97f04d53604b84c8ae21b
KiranVK <34923686+KiranVK98@users.noreply.github.com>
2022-06-16 13:39:46 -0500

    quiz2

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import pyodbc
+import textwrap
+from azure.storage.blob import BlobServiceClient, ContentSettings
+server = 'kiran98.database.windows.net'
+database = 'Assignment12'
+username = 'kiran1998'
+password = 'Omsrn@062466'
+driver = '{ODBC Driver 18 for SQL Server}'
+
+app = Flask(__name__)
+
+sqlconnection = pyodbc.connect('DRIVER='+driver+';SERVER='+server +
+                               ';PORT=1433;DATABASE='+database+';UID='+username+';PWD=' + password)
+cursor = sqlconnection.cursor()
+
+
+connection_string = "DefaultEndpointsProtocol=https;AccountName=assigns1;AccountKey=FjfZ2UGw8oZx9cDaz2PJYqCtqMlyAXVuGt5Dq8TTcN1InDs8yUrgc8PIu48Xq8A7zku1SP0G+1hN+AStHhKtsQ==;EndpointSuffix=core.windows.net"
+img_container = "uniqcontain"
+
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/lat', methods=["POST", "GET"])
+def lat():
+    lat = str(request.form.get("lat"))
+    degrees = str(request.form.get("long"))
+    deviation_away = float(lat) - float(degrees)
+    deviation_towards = float(lat) + float(degrees)
+    ds = "SELECT time,latitude FROM [dbo].[eq] WHERE latitude > {} and latitude < {}".format(
+        deviation_away, deviation_towards)
+    cursor.execute(ds)
+    ftch = cursor.fetchall()
+    print(ftch)
+    return render_template('lat.html', dsp=ftch)
+
+
+if __name__ == '__main__':  # only run if you run this file, not if you import other main.py file
+    #os.environ['PYTHONPATH'] = os.getcwd()
+    app.run(debug=True)
